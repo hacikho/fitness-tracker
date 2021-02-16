34d37bc87e9deb04fa6aea58f32df0595393f6fe
Haci Karahasanoglu <haci@hacikho.com>
2021-02-16 15:16:30 -0500

    Current, new and past training modules addes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,13 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 
 @NgModule({
@@ -20,7 +27,13 @@ import {MatToolbarModule} from '@angular/material/toolbar';
         MatNativeDateModule, 
         MatCheckboxModule, 
         MatSidenavModule,
-        MatToolbarModule ],
+        MatToolbarModule,
+        MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatSelectModule,
+        MatProgressSpinnerModule,
+        MatDialogModule ],
     exports:[
         MatButtonModule,
         MatIconModule, 
@@ -30,6 +43,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
         MatNativeDateModule, 
         MatCheckboxModule, 
         MatSidenavModule,
-        MatToolbarModule ]
+        MatToolbarModule,
+        MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatSelectModule,
+        MatProgressSpinnerModule,
+        MatDialogModule ]
 })
 export class MaterialModule{}
